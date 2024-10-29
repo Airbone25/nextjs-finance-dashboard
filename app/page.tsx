46bd91ce7e9dd8af98b7d49b8836e864162f07a9
Keshav <keshavmehra2005@gmail.com>
2024-10-29 23:37:28 +0530

    Small Changes made for production

--- app/page.tsx
+++ app/page.tsx
@@ -25,9 +25,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image src="/hero-desktop.png" width={1000} height={760} className='hidden md:block' alt='img'/>
-        </div>
+        
       </div>
       
     </main>
